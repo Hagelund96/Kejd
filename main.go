0e724c4935130c05ed2ef1912c8d4cebf999b812
Hagelund96 <hagelund96@hotmail.com>
2018-10-13 19:54:29 +0200

    POST /api/igc should now work

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/marni/goigc"
 )
 
 const (
@@ -18,7 +20,18 @@ type Information struct {
 	Version		string	`json:"version"`
 }
 
+type Track struct {
+	ID 			int			`json:"ID"`
+	HeaderDate  time.Time 	`json:"Header date"`
+	Pilot       string 		`json:"Pilot"`
+	Glider      string 		`json:"Glider"`
+	GliderId    string 		`json:"Glider id"`
+	TrackLength float64		`json:"Track length"`
+}
+
 var startTime time.Time
+var tracks map[int]Track
+var ID int
 
 func init(){
 	startTime = time.Now()
@@ -42,9 +55,41 @@ func handlerApi(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func handlerIgc(w http.ResponseWriter, r *http.Request){
+	switch r.Method{
+	case("POST"):
+		var url string
+		err := json.NewDecoder(r.Body).Decode(&url)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		track, err := igc.ParseLocation(url)
+		tracks[ID] = Track{ID, track.Date, track.Pilot, track.GliderType, track.GliderID, CalculatedDistance(track)}
+		post, err := json.Marshal(tracks[ID].ID)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		http.Header.Add(w.Header(), "content-type", "application/json")
+		json.NewEncoder(w).Encode(post)
+		ID++
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func CalculatedDistance(track igc.Track) float64 {
+	distance := 0.0
+	for i := 0; i < len(track.Points)-1; i++ {
+		distance += track.Points[i].Distance(track.Points[i+1])
+	}
+	return distance
+}
+
 func main(){
 	http.HandleFunc("/igcinfo/api/", handlerApi)
-
+	http.HandleFunc("/igcinfo/api/igc", handlerIgc)
 
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
